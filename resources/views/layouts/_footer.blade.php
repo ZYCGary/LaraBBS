26f6da3b577fbf44dce750083b2206b3e879598c
ZYCGary <zyc.gary@gmail.com>
2019-11-23 00:02:38 +1100

    Edit basic page layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Designed & Developed By <a href="https://www.linkedin.com/in/garyzhang53/" target="_blank">Gary</a> <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:gary138201920202@gmail.com">Contact Us</a></p>
+  </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'HOME')
+
+@section('content')
+  <h1>Welcome to home page.</h1>
+@stop
