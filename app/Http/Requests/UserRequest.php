22db3e571e68ae00ee281778de9e2ef8b8fea3a3
ZYCGary <zyc.gary@gmail.com>
2019-11-24 15:36:52 +1100

    Set min resolution of avatar image upload

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
     /**
@@ -40,8 +41,10 @@ class UserRequest extends FormRequest
         return [
             'name.unique' => 'The username is token, please try another one.',
             'name.regex' => 'Invalid username, only chars, numbers, "-" and "_" are available.',
-            'name.between' => 'The length of username should range between 3 - 25.',
+            'name.between' => 'The length of username must range between 3 - 25.',
             'name.required' => 'Username can\'t be null.',
+            'avatar.mimes' =>'Your avatar must be an image in format of jpeg, bmp, png or gif',
+            'avatar.dimensions' => 'Your avatar\'s resolution must larger than 208px * 208px',
         ];
     }
 }
