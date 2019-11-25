e13dda7e94de9136d13b4f21cd67dab6fec1818e
ZYCGary <zyc.gary@gmail.com>
2019-11-26 09:11:10 +1100

    Add WYSIWYG Editor (Simditor)

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,57 +2,80 @@
 
 @section('content')
 
-<div class="container">
-  <div class="col-md-10 offset-md-1">
-    <div class="card ">
+  <div class="container">
+    <div class="col-md-10 offset-md-1">
+      <div class="card ">
 
-      <div class="card-body">
-        <h2 class="">
-          <i class="far fa-edit"></i>
-          @if($topic->id)
-          Edit Topic
-          @else
-          New Topic
-          @endif
-        </h2>
-
-        <hr>
-
-        @if($topic->id)
-        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-          <input type="hidden" name="_method" value="PUT">
-          @else
-          <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+        <div class="card-body">
+          <h2 class="">
+            <i class="far fa-edit"></i>
+            @if($topic->id)
+              Edit Topic
+            @else
+              New Topic
             @endif
+          </h2>
 
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <hr>
 
-            @include('shared._error')
+          @if($topic->id)
+            <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+              <input type="hidden" name="_method" value="PUT">
+              @else
+                <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+                  @endif
 
-            <div class="form-group">
-              <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="Your Title Here" required />
-            </div>
+                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-            <div class="form-group">
-              <select class="form-control" name="category_id" required>
-                <option value="" hidden disabled selected>Select Category</option>
-                @foreach ($categories as $value)
-                <option value="{{ $value->id }}">{{ $value->name }}</option>
-                @endforeach
-              </select>
-            </div>
+                  @include('shared._error')
 
-            <div class="form-group">
-              <textarea name="body" class="form-control" id="editor" rows="6" placeholder="Your content here" required>{{ old('body', $topic->body ) }}</textarea>
-            </div>
+                  <div class="form-group">
+                    <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}"
+                           placeholder="Your Title Here" required/>
+                  </div>
 
-            <div class="well well-sm">
-              <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i>Save</button>
-            </div>
-          </form>
+                  <div class="form-group">
+                    <select class="form-control" name="category_id" required>
+                      <option value="" hidden disabled selected>Select Category</option>
+                      @foreach ($categories as $value)
+                        <option value="{{ $value->id }}">{{ $value->name }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <textarea name="body" class="form-control" id="editor" rows="6" placeholder="Your content here"
+                              required>{{ old('body', $topic->body ) }}</textarea>
+                  </div>
+
+                  <div class="well well-sm">
+                    <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i>Save
+                    </button>
+                  </div>
+                </form>
+            </form>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-@endsection
+@stop
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+      $(document).ready(function () {
+          var editor = new Simditor({
+              textarea: $('#editor'),
+          });
+      });
+  </script>
+@stop
